90927473a5de56ded836e833af69ffcbb5d9ecb1
XJINE <xjine.andorelseif@gmail.com>
2020-08-26 18:31:47 +0900

    First commit.

---
+++ Assets/Packages/IPAddressDrivenSceneLoader/IPAddressDrivenSceneLoader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IPAddressDrivenSceneLoader : SingletonMonoBehaviour<IPAddressDrivenSceneLoader>
+{
+    [System.Serializable]
+    public class SceneInfo
+    {
+        public string        ipAddress;
+        public string        sceneName;
+        public LoadSceneMode loadMode;
+    }
+
+    #region Field
+
+    public SceneInfo[] sceneInfos;
+
+    protected bool singleSceneLoaded;
+
+    #endregion Field
+
+    #region Method
+
+    // NOTE:
+    // Set into IPAddressNotifier.observers.handler.
+
+    public virtual void IPAddressEventHandler(IPAddress ipAddress)
+    {
+        // NOTE:
+        // Need to avoid load single-scene twice.
+
+        try
+        {
+            SceneInfo sceneInfo = this.sceneInfos.FirstOrDefault(info => info.ipAddress == ipAddress.ToString());
+
+            if (sceneInfo == null || this.singleSceneLoaded)
+            {
+                return;
+            }
+
+            this.singleSceneLoaded = sceneInfo.loadMode == LoadSceneMode.Single;
+
+            SceneManager.LoadScene(sceneInfo.sceneName, sceneInfo.loadMode);
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    #endregion Method
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Unity_IPAddressDrivenSceneLoader
+
+<img src="https://github.com/XJINE/Unity_IPAddressDrivenSceneLoader/blob/master/Screenshot.png" width="100%" height="auto" />
+
+``IPAddressDrivenSceneLoader`` select and load your scene from IPAddress.
+
+## Import to Your Project
+
+You can import this asset from UnityPackage.
+
+- [IPAddressDrivenSceneLoader.unitypackage](https://github.com/XJINE/Unity_IPAddressDrivenSceneLoader/blob/master/IPAddressDrivenSceneLoader.unitypackage)
+
+### Dependencies
+
+You have to import following assets to use this asset.
+
+- [IPAddressNotifier](https://github.com/XJINE/Unity_IPAddressNotifier)
+- [SingletonMonoBehaviour](https://github.com/XJINE/Unity_SingletonMonoBehaviour)
+
+## How to Use
+
+1. Set ``IPAddressDrivenSceneLoader.IPAddressEventHandler`` into ``IPAddressNotifier.Observers.Handler``.
+2. Set some ``IPAddress`` and the related ``SceneName`` from Inspector.
